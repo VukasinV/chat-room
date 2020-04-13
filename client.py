8d0238a8c965ffdea4bc5b01b68a080cc101cd8c
--set <vrani.vukasin@gmail.com>
2020-04-13 19:01:14 +0200

    changed to object-oriented classes

--- client.py
+++ client.py
@@ -1,35 +1,25 @@
 import socket
-
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.8"
-ADDR = (SERVER, PORT)
+import threading
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+class Client:
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# sends message to the server
+    def __init__(self):
+        print("[CLIENT] Starting...")
+        self.socket.connect(('192.168.1.8', 10000))
+        iThread = threading.Thread(target=self.sendMsg)
+        iThread.daemon = True
+        iThread.start()
+        while True:
+            data = self.socket.recv(1024)
+            if not data:
+                break
+            print(data)
+
+    def sendMsg(self):
+        while True:
+            self.socket.send(bytes(input(""), 'utf-8'))
 
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-
-
-send("Hello world")
-chat = True
-while chat:
-    print("Message (to cancel type 'xx'): ")
-    message_to_send = input()
-    if message_to_send == 'xx':
-        chat = False
-        break
-        print("You have exited the chat")
-    send(message_to_send)
+client = Client()
--- server.py
+++ server.py
@@ -1,54 +1,35 @@
 import socket
 import threading
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+class Server:
+    SERVER = socket.gethostbyname(socket.gethostname())
+    PORT = 10000
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
 
-# need to keep track of clients, which means it needs to be a list of clients
-# clients = []
+    def __init__(self):
+        self.socket.bind((self.SERVER, self.PORT))
+        self.socket.listen(1)
 
+    def handler(self, client, address):
+        while True:
+            data = client.recv(1024)
+            for connection in self.connections:
+                connection.send(data)
+            if not data:
+                break
 
-def handle_client(connection, address):
-    print(f"[NEW CONNECTION] {address} connected")
-    connected = True
-    while connected:
-        # why we do need to recieve a header? and length of a message???
-        msg_length = connection.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = connection.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{address}] {msg}")
-
-    connection.close()
-
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        connection, address = server.accept()
-        thread = threading.Thread(
-            target=handle_client, args=(connection, address))
-        thread.start()
-        print(f"[ACTIVE CONNECTION] {threading.activeCount() - 1}")
-
-# TODO: send message to all clients: check if you can access threading arguments
-# and send message to that connection
+    def run(self):
+        while True:
+            print("[SERVER] Staring...")
+            print("[SERVER] Listening...")
+            conn, addr = self.socket.accept()
+            cThread = threading.Thread(target=self.handler, args=(conn, addr))
+            cThread.daemon = True
+            cThread.start()
+            self.connections.append(conn)
 
 
-def send_message(message):
-    pass
-
-
-print("[STARTING] server is starting...")
-start()
+server = Server()
+server.run()
